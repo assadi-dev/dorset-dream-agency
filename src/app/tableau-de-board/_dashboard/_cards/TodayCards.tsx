57661f6382591f932c20c50b1194aafb57933789
assadi-dev <assadi.halifa@gmail.com>
2024-10-01 01:33:10 +0200

    Conversion composant en server affichage date

--- src/app/tableau-de-board/_dashboard/_cards/TodayCards.tsx
+++ src/app/tableau-de-board/_dashboard/_cards/TodayCards.tsx
@@ -1,27 +1,15 @@
 "use client";
 import React from "react";
 import { getDateToday } from "../helper";
-import { useInterval } from "@/hooks/useInterval";
 import DashboardCard from "../DashboardCard";
-import { TodayDateReducerType } from "./types";
 
-const reducer = (prev: TodayDateReducerType, state: TodayDateReducerType) => ({ ...prev, ...state });
+import ClientHoursTick from "./ClientClock";
+
 const TodayCards = () => {
-    const [state, setState] = React.useReducer(reducer, { day: "", hours: "", week: 0 });
-
-    const updateDate = React.useCallback(() => {
-        const { day, hours, week } = getDateToday();
-        setState({ day: day.toUpperCase(), hours: hours.toUpperCase(), week: week });
-    }, []);
-
-    useInterval(updateDate, 1000);
+    const { day, hours, week } = getDateToday();
     return (
         <DashboardCard className="bg-primary text-secondary">
-            <div className="text-center grid grid-rows-[auto,1fr,auto] gap-3 justify-content-center">
-                <p className="font-bold">{state.day}</p>
-                <p className="text-3xl font-bold">{state.hours}</p>
-                <p className="font-bold">SEMAINE {state.week}</p>
-            </div>
+            <ClientHoursTick day={day.toUpperCase()} hours={hours.toUpperCase()} week={week} />
         </DashboardCard>
     );
 };
