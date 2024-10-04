1573506d3439edb5c03fa8d44fcb7915ee5908c6
assadi-dev <assadi.halifa@gmail.com>
2024-10-05 01:20:05 +0200

    ajustement grid client

--- src/app/tableau-de-board/gestion-des-clients/_components/ClientCard.tsx
+++ src/app/tableau-de-board/gestion-des-clients/_components/ClientCard.tsx
@@ -9,16 +9,21 @@ const ClientCard = ({ client }: { client: ClientType }) => {
     const UNKNOWN_IMAGE = client.gender === "Female" ? AVATAR_FEMALE : AVATAR_MALE;
     return (
         <Link href={{ pathname: "gestion-des-clients/detail", query: { client: client.slug } }}>
-            <Card className="w-[200px] shadow-xl">
-                <figure>
-                    <Image src={UNKNOWN_IMAGE} alt={`photo of ${client.name}`} />
-                    <figcaption className="bg-white p-3 text-center leading-8">
-                        <p>{client.fullName}</p>
-                        <p>{client.phone}</p>
-                        <p>{client.createdAt}</p>
-                    </figcaption>
-                </figure>
-            </Card>
+            <figure className="w-full shadow rounded  bg-white h-[150px] relative grid">
+                <Image
+                    width={100}
+                    height={100}
+                    src={UNKNOWN_IMAGE}
+                    alt={`photo of ${client.name}`}
+                    className="w-[100px] rounded-l-lg absolute top-[50%] translate-y-[-50%] left-0  z-0 opacity-25"
+                />
+
+                <figcaption className=" text-sm text-center leading-6 w-full  lg:self-center z-10 lg:ml-8">
+                    <p className="font-bold">{client.fullName}</p>
+                    <p>{client.phone}</p>
+                    <p>{client.createdAt}</p>
+                </figcaption>
+            </figure>
         </Link>
     );
 };
--- src/app/tableau-de-board/gestion-des-clients/_components/ListeClients.tsx
+++ src/app/tableau-de-board/gestion-des-clients/_components/ListeClients.tsx
@@ -7,7 +7,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 const ListeClients = () => {
     return (
         <ScrollArea className="h-[calc(85vh-200px)] rounded ">
-            <div className="grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,200px)] justify-between  gap-x-5 gap-y-5">
+            <div className="grid grid-cols-2 lg:grid-cols-[repeat(auto-fit,350px)] justify-center gap-x-9 gap-y-9">
                 {cleanClientMock.map((client) => (
                     <ClientCard key={client.id} client={client} />
                 ))}
