ff891f9c5b30649a0526732a9e18fb3c4f8dc157
assadi-dev <assadi.halifa@gmail.com>
2024-10-09 21:19:52 +0200

    format

--- src/HOC/ButtonWithToolTip.tsx
+++ src/HOC/ButtonWithToolTip.tsx
@@ -2,23 +2,23 @@ import React from "react";
 import { Tooltip } from "react-tooltip";
 
 const ButtonWithToolTip = (Component, tooltipContent, tooltipClassName) => {
-  const NewComponent = ({ ...props }) => {
-    return (
-      <>
-        <span className="label-tooltip" data-tooltip-content={tooltipContent}>
-          <Component {...props} />
-        </span>
-        <Tooltip
-          className={tooltipClassName}
-          anchorSelect=".label-tooltip"
-          place="bottom"
-          style={{ zIndex: 10 }}
-        />
-      </>
-    );
-  };
+    const NewComponent = ({ ...props }) => {
+        return (
+            <>
+                <span className="label-tooltip" data-tooltip-content={tooltipContent}>
+                    <Component {...props} />
+                </span>
+                <Tooltip
+                    className={tooltipClassName}
+                    anchorSelect=".label-tooltip"
+                    place="bottom"
+                    style={{ zIndex: 10 }}
+                />
+            </>
+        );
+    };
 
-  return (props) => <NewComponent {...props} />;
+    return (props) => <NewComponent {...props} />;
 };
 
 export default ButtonWithToolTip;
