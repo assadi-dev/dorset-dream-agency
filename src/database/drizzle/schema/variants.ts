c41aa4e2f81906c8927c73b2413aa89bdd15cd2c
assadi-dev <assadi.halifa@gmail.com>
2024-10-13 19:59:50 +0200

    ajout relation schema

--- src/database/drizzle/schema/variants.ts
+++ src/database/drizzle/schema/variants.ts
@@ -2,18 +2,17 @@ import { updatedAndCreatedAt } from "../utils";
 import { int, mysqlEnum, mysqlTable, text, varchar } from "drizzle-orm/mysql-core";
 import { properties } from "./properties";
 import { relations } from "drizzle-orm";
-import { photos } from "./photos";
+import { galleryVariants } from "./galleryVariant";
 
 export const variants = mysqlTable("variants", {
     id: int("id").autoincrement().primaryKey(),
     name: varchar("name", { length: 100 }),
     description: text("description"),
-    type: mysqlEnum("type", ["Location", "Vente"]).notNull(),
     propertyID: int("property_id").references(() => properties.id),
     ...updatedAndCreatedAt,
 });
 
-export const variantsRelation = relations(variants, ({ one }) => ({
+export const variantsRelation = relations(variants, ({ one, many }) => ({
     property: one(properties, { fields: [variants.propertyID], references: [properties.id] }),
-    // photo: one(photos, { fields: [variants.photoID], references: [photos.id] }),
+    photos: many(galleryVariants),
 }));
