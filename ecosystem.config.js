48d15ccd5354f18e0f982138cf7384d85e40b465
assadi-dev <assadi.halifa@gmail.com>
2024-10-20 22:20:13 +0200

    config pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+        {
+            name: "dorset-dream-agency",
+            script: "npm",
+            args: "start",
+            watch: true,
+            instances: 1,
+            max_memory_restart: "500M",
+            restart_delay: 1000,
+            env: {
+                NODE_ENV: "production",
+            },
+        },
+    ],
+};
