8e41ad929bd6703bee0e16c029ee5402d0694cbb
assadi-dev <assadi.halifa@gmail.com>
2024-10-21 22:27:15 +0200

    add suspense page connexion

--- src/app/connexion/page.tsx
+++ src/app/connexion/page.tsx
@@ -2,7 +2,6 @@ import { setTitlePage } from "@/lib/utils";
 import React from "react";
 import backgroundImage from "@assets/images/background.jpg";
 import LoginForm from "./LoginForm";
-import { Toaster } from "sonner";
 
 export const metadata = setTitlePage("Connexion");
 const Connexion = async () => {
@@ -15,19 +14,21 @@ const Connexion = async () => {
     };
 
     return (
-        <main style={backgroundStyle} className="lg:grid lg:grid-cols-[1fr,0.5fr]">
-            <section></section>
-            <section
-                className=" md:grid md:place-items-center text-secondary "
-                style={{
-                    background: "#0b1b47dc",
-                    backdropFilter: "blur(8px)",
-                    borderLeft: "1px solid rgba(255,255,255,0.3)",
-                }}
-            >
-                <LoginForm />
-            </section>
-        </main>
+        <React.Suspense>
+            <main style={backgroundStyle} className="lg:grid lg:grid-cols-[1fr,0.5fr]">
+                <section></section>
+                <section
+                    className=" md:grid md:place-items-center text-secondary "
+                    style={{
+                        background: "#0b1b47dc",
+                        backdropFilter: "blur(8px)",
+                        borderLeft: "1px solid rgba(255,255,255,0.3)",
+                    }}
+                >
+                    <LoginForm />
+                </section>
+            </main>
+        </React.Suspense>
     );
 };
 
