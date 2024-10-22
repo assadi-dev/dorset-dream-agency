f384a00d873f0fdb0bf7e81591f3539d5535f491
assadi-dev <assadi.halifa@gmail.com>
2024-10-22 23:15:00 +0200

    Added `refetchOnMount: true` to `useQuery` options in `ListProperties` component.

--- src/app/tableau-de-board/gestion-des-biens-immobiliers/_components/ListProperties.tsx
+++ src/app/tableau-de-board/gestion-des-biens-immobiliers/_components/ListProperties.tsx
@@ -14,6 +14,7 @@ const ListProperties = () => {
     const { data, isFetching, error } = useQuery({
         queryKey: ["LIST_IMMOBILIER_GESTION"],
         queryFn: fetchPropertiesCollections,
+        refetchOnMount: true,
     });
 
     const actions = {
