7dfa8a29904f9ba5408cb1a6c70d5104d517efa0
assadi-dev <assadi.halifa@gmail.com>
2024-10-22 22:51:22 +0200

    add header caches

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,4 +2,9 @@ import axios from "axios";
 
 export const API_INSTANCE = axios.create({
     baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
+    headers: {
+        "Cache-Control": "no-cache",
+        Pragma: "no-cache",
+        Expires: "0",
+    },
 });
