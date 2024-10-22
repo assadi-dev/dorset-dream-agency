2d76beb6fbd57e968c5734dc05356638edac8a77
assadi-dev <assadi.halifa@gmail.com>
2024-10-23 01:44:56 +0200

    ajout refresh apres supression proprieté

--- src/app/tableau-de-board/gestion-des-biens-immobiliers/_components/forms/DeleteProperty.tsx
+++ src/app/tableau-de-board/gestion-des-biens-immobiliers/_components/forms/DeleteProperty.tsx
@@ -1,16 +1,24 @@
+"use client";
 import AlertModalContent from "@/components/Modals/AlertModalContent";
 import { ToastErrorSonner, ToastSuccessSonner } from "@/components/notify/Sonner";
 import { deleteVariant } from "@/database/drizzle/repositories/variants";
 import useModalState from "@/hooks/useModalState";
+import { usePathname, useRouter } from "next/navigation";
+
 import React from "react";
 
 const DeleteProperty = () => {
     const { payload, closeModal } = useModalState();
+    const pathname = usePathname();
+    const router = useRouter();
+
     const handleConfirm = async () => {
         try {
             await deleteVariant([payload.id]);
             ToastSuccessSonner(`la propriété ${payload.name} a été supprimé avec succès`);
             closeModal();
+            router.push(pathname);
+            router.refresh();
         } catch (error: any) {
             ToastErrorSonner(`La suppression n'a pas pu être effectué raison: ${error.message}`);
         }
