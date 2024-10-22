82df83bb56e7c0d7b966ecba2d13fb28382c0b61
assadi-dev <assadi.halifa@gmail.com>
2024-10-22 23:37:31 +0200

    add force dynamic api route

--- src/app/api/(variants)/variants/collections/route.ts
+++ src/app/api/(variants)/variants/collections/route.ts
@@ -2,6 +2,8 @@ import { getVariantsCollections } from "@/database/drizzle/repositories/variants
 import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export const GET = async (req: Request) => {
     try {
         const variants = await getVariantsCollections();
