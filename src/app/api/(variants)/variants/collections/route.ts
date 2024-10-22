bd42745e3c9c028fa3e211baf9b01c6d65b144f6
assadi-dev <assadi.halifa@gmail.com>
2024-10-22 22:36:05 +0200

    "Updated API routes and added revalidation, refactored AddProperty component to use useRouter and usePathname"

--- src/app/api/(variants)/variants/collections/route.ts
+++ src/app/api/(variants)/variants/collections/route.ts
@@ -1,7 +1,8 @@
 import { getVariantsCollections } from "@/database/drizzle/repositories/variants";
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
-export const GET = async () => {
+export const GET = async (req: Request) => {
     try {
         const variants = await getVariantsCollections();
         return NextResponse.json(variants);
--- src/app/api/(variants)/variants/gallery/route.ts
+++ src/app/api/(variants)/variants/gallery/route.ts
@@ -1,10 +1,12 @@
 import { createVariantGallery } from "@/database/drizzle/repositories/galleries";
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export const POST = async (request: Request) => {
     try {
         const formData = await request.formData();
         await createVariantGallery(formData);
+        revalidatePath(request.url);
         return NextResponse.json({
             message: `Create gallery success`,
         });
