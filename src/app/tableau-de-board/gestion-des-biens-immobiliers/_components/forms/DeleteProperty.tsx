73f7ca0d36c9648b8156804bdcef8c2728d2d403
assadi-dev <assadi.halifa@gmail.com>
2024-10-23 01:58:52 +0200

    refetch apres action suppression

--- src/app/tableau-de-board/gestion-des-biens-immobiliers/_components/forms/DeleteProperty.tsx
+++ src/app/tableau-de-board/gestion-des-biens-immobiliers/_components/forms/DeleteProperty.tsx
@@ -3,22 +3,21 @@ import AlertModalContent from "@/components/Modals/AlertModalContent";
 import { ToastErrorSonner, ToastSuccessSonner } from "@/components/notify/Sonner";
 import { deleteVariant } from "@/database/drizzle/repositories/variants";
 import useModalState from "@/hooks/useModalState";
-import { usePathname, useRouter } from "next/navigation";
+import { useQueryClient } from "@tanstack/react-query";
 
 import React from "react";
 
 const DeleteProperty = () => {
     const { payload, closeModal } = useModalState();
-    const pathname = usePathname();
-    const router = useRouter();
+
+    const queryClient = useQueryClient();
 
     const handleConfirm = async () => {
         try {
-            await deleteVariant([payload.id]);
+            await deleteVariant([Number(payload.id)]);
             ToastSuccessSonner(`la propriété ${payload.name} a été supprimé avec succès`);
+            queryClient.invalidateQueries({ queryKey: ["LIST_IMMOBILIER_GESTION"] });
             closeModal();
-            router.push(pathname);
-            router.refresh();
         } catch (error: any) {
             ToastErrorSonner(`La suppression n'a pas pu être effectué raison: ${error.message}`);
         }
