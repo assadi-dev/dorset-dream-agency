a8b4520613804217e5163f27f5aaecf029ddb659
assadi-dev <assadi.halifa@gmail.com>
2024-10-27 14:50:00 +0100

    recuperation des info category par son nom

---
+++ src/app/api/(categoryProperty)/category/[name]/route.ts
@@ -0,0 +1,19 @@
+import { getCategoryByName } from "@/database/drizzle/repositories/categories";
+import { NextResponse } from "next/server";
+
+type Params = {
+    params: {
+        name: string;
+    };
+};
+export async function GET(req: Request, { params: { name } }: Params) {
+    try {
+        const result = await getCategoryByName(name);
+        return NextResponse.json(result);
+    } catch (error: any) {
+        if (error instanceof Error)
+            return NextResponse.json({
+                error: error.message,
+            });
+    }
+}
