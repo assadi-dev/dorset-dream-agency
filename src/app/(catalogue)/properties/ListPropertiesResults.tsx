31d41c605fcbcf8d6f129877a48012e84c558212
assadi-dev <assadi.halifa@gmail.com>
2024-11-03 02:12:43 +0100

    responsive page recherche

--- src/app/(catalogue)/properties/ListPropertiesResults.tsx
+++ src/app/(catalogue)/properties/ListPropertiesResults.tsx
@@ -5,7 +5,7 @@ const ListPropertiesResultsSection = () => {
     const array = new Array(25).fill({ name: "sdsdd" });
     return (
         <section className="py-6">
-            <div className="lg:grid grid-cols-[repeat(auto-fill,25.6rem)] lg:grid-rows-[repeat(14rem)] gap-5 justify-between">
+            <div className="grid grid-cols-[repeat(auto-fill,minmax(23rem,1fr))] lg:grid-cols-[repeat(auto-fill,minmax(25rem,1fr))] grid-rows-[repeat(14rem)] gap-5">
                 {array.map((item, index) => (
                     <PropertyCard key={index} />
                 ))}
