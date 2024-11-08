0f5fa47e7018bd0ffc61c0a46aa9753d68f949b7
assadi-dev <assadi.halifa@gmail.com>
2024-11-08 01:00:41 +0100

    affichage non achetable

--- src/app/(catalogue)/_components/main/PropertyCard.tsx
+++ src/app/(catalogue)/_components/main/PropertyCard.tsx
@@ -23,6 +23,15 @@ type PropertyCardProps = {
     property: PropertyItemType;
 };
 const PropertyCard = ({ property }: PropertyCardProps) => {
+    const sellingPrice =
+        property.sellingPrice !== -1 ? (
+            property.sellingPrice + "$"
+        ) : (
+            <span className="px-2 py-1 ring-1 ring-destructive rounded text-xs lg:text-sm  text-destructive bg-red-100">
+                Non achetable
+            </span>
+        );
+
     return (
         <Card className="w-full p-1 h-full transition-shadow hover:shadow-lg relative ">
             <div className="overflow-hidden rounded-lg relative h-[180px] lg:h-[250px]">
@@ -40,17 +49,17 @@ const PropertyCard = ({ property }: PropertyCardProps) => {
                     {property.name}{" "}
                 </p>
                 <PropertyBadges isAvailable={property.isAvailable} isFurnish={property.isFurnish} />
-                <div className="flex items-center justify-between  p-2 rounded-sm backdrop-blur-md">
-                    <div>
+                <div className="flex flex-col lg:flex-row items-center justify-between  p-2 rounded-sm backdrop-blur-md w-full gap-3">
+                    <div className="flex flex-col gap-1">
                         <p className="text-xs text-slate-600">Location - Vente</p>
                         <p className="font-bold text-xs lg:text-lg">
-                            {property.rentalPrice}$ - {property.sellingPrice}${" "}
+                            {property.rentalPrice}$ - {sellingPrice}{" "}
                         </p>
                     </div>
                     <Button
                         asChild
                         type="button"
-                        className="bg-background border hover:border-none hover:shadow-lg hover:shadow-blue-950/50 border-input text-black hover:bg-gradient-to-br from-sky-600 to-primary   duration-300   transition-colors  hover:text-white text-xs lg:text-sm"
+                        className="bg-background border hover:border-none hover:shadow-lg hover:shadow-blue-950/50 border-input text-black hover:bg-gradient-to-br from-sky-600 to-primary   duration-300   transition-colors  hover:text-white text-xs lg:text-sm w-full lg:w-fit"
                     >
                         <Link href={`property?id=${property.id}&name=${property.name}`}>Plus d'info</Link>
                     </Button>
