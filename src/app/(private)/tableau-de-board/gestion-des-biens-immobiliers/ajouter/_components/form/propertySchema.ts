669cd9cdfd526550f45b18de9cf42160147f35bb
assadi-dev <assadi.halifa@gmail.com>
2024-11-11 12:16:36 +0100

    Ajout propriété id

--- src/app/(private)/tableau-de-board/gestion-des-biens-immobiliers/ajouter/_components/form/propertySchema.ts
+++ src/app/(private)/tableau-de-board/gestion-des-biens-immobiliers/ajouter/_components/form/propertySchema.ts
@@ -2,6 +2,7 @@ import { MAX_LENGTH_MESSAGE, REQUIRE_MESSAGE_ERROR } from "@/config/messages";
 import { z } from "zod";
 
 export const propertySchema = z.object({
+    id: z.number().nullable().optional(),
     name: z.string({ message: REQUIRE_MESSAGE_ERROR }).min(1, { message: REQUIRE_MESSAGE_ERROR }),
     resume: z.string().max(250, { message: MAX_LENGTH_MESSAGE }).nullable().optional(),
     description: z.string().nullable(),
