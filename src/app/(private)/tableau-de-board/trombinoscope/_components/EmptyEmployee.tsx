95187f447fe8b083e204d1645c578961abdf577f
assadi-dev <assadi.halifa@gmail.com>
2024-11-17 22:51:14 +0100

    "Added 'use client' directive and search parameter handling to EmptyEmployee component"

--- src/app/(private)/tableau-de-board/trombinoscope/_components/EmptyEmployee.tsx
+++ src/app/(private)/tableau-de-board/trombinoscope/_components/EmptyEmployee.tsx
@@ -1,9 +1,21 @@
+"use client";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 const EmptyEmployee = () => {
+    const searchParam = useSearchParams();
+    const search = searchParam.get("search");
+
+    const EMPTY_MESSAGE = search ? (
+        <p>
+            Aucun employé trouvé correspondant à la recherche <strong className="mx-1">{search}</strong>
+        </p>
+    ) : (
+        `Aucun employé enregistré`
+    );
     return (
         <div className="w-full flex justify-center p-3">
-            <p className="text-gray-500">Aucun employé enregistré</p>
+            <p className="text-gray-500">{EMPTY_MESSAGE}</p>
         </div>
     );
 };
