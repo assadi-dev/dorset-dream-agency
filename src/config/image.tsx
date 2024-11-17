f03b9b3db507c88e99cb7f14f298a5d0694b9a71
assadi-dev <assadi.halifa@gmail.com>
2024-11-17 23:14:14 +0100

    optimisation images

--- src/config/image.tsx
+++ src/config/image.tsx
@@ -3,3 +3,6 @@ import unknownFemale from "@assets/images/unknown-female.jpg";
 
 export const AVATAR_MALE = unknownMale;
 export const AVATAR_FEMALE = unknownFemale;
+
+export const SAN_ANDREAS_IMAGE = "@assets/images/san-andreas-logo.jpg";
+export const ILES_GALAPAGOS_IMAGE = "@assets/images/iles-galapagos-logo.jpg";
