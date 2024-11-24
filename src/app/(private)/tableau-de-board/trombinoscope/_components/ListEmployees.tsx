9f9150c962550d3c4c0fb1202ae944670074b60f
assadi-dev <assadi.halifa@gmail.com>
2024-11-24 17:37:57 +0100

    pagination trombi

--- src/app/(private)/tableau-de-board/trombinoscope/_components/ListEmployees.tsx
+++ src/app/(private)/tableau-de-board/trombinoscope/_components/ListEmployees.tsx
@@ -4,28 +4,37 @@ import EmployeeCard from "./EmployeeCard";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { EmployeeBasic } from "@/app/types/employee";
 import EmptyEmployee from "./EmptyEmployee";
+import SimplePagination from "@/components/Paginations/SimplePagination";
 
 type ListEmployeesProps = {
     employees?: EmployeeBasic[];
+    totalItems: number;
+    limit: number;
 };
-const ListEmployees = ({ employees }: ListEmployeesProps) => {
+const ListEmployees = ({ employees, totalItems, limit }: ListEmployeesProps) => {
     const EMPLOYEES_LIST = React.useMemo(() => {
         if (!employees) return [];
         return employees;
     }, [employees]);
 
     return (
-        <ScrollArea className="py-6 px-3 h-[72vh]">
-            {EMPLOYEES_LIST.length > 0 ? (
-                <div className="grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] grid-rows-[repeat(auto-fill,380px)] gap-3 w-full h-full">
-                    {EMPLOYEES_LIST.map((employee) => (
-                        <EmployeeCard key={employee.id} employee={employee} />
-                    ))}
-                </div>
-            ) : (
-                <EmptyEmployee />
-            )}
-        </ScrollArea>
+        <>
+            <div className="flex justify-between">
+                <div></div>
+                <SimplePagination limit={limit} totalItems={totalItems} />
+            </div>
+            <ScrollArea className="py-6 px-3 h-[72vh]">
+                {EMPLOYEES_LIST.length > 0 ? (
+                    <div className="grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] grid-rows-[repeat(auto-fill,380px)] gap-3 w-full h-full">
+                        {EMPLOYEES_LIST.map((employee) => (
+                            <EmployeeCard key={employee.id} employee={employee} />
+                        ))}
+                    </div>
+                ) : (
+                    <EmptyEmployee />
+                )}
+            </ScrollArea>
+        </>
     );
 };
 
--- src/app/(private)/tableau-de-board/trombinoscope/page.tsx
+++ src/app/(private)/tableau-de-board/trombinoscope/page.tsx
@@ -6,22 +6,40 @@ import LoadingSkeleton from "./_components/LoadingSkeleton";
 import PageTemplate from "../_components/PageTemplate";
 import { getEmployeeCollections } from "@/database/drizzle/repositories/employee";
 import { EmployeeBasic } from "@/app/types/employee";
+import SimplePagination from "@/components/Paginations/SimplePagination";
 
 export const metadata = setTitlePage("Prestige");
 type TrombinoscopePageProps = {
-    searchParams: { search: string };
+    searchParams: { search: string; limit: string; page: string };
 };
-const TrombinoscopePage = async ({ searchParams: { search } }: TrombinoscopePageProps) => {
+const TrombinoscopePage = async ({ searchParams }: TrombinoscopePageProps) => {
+    const search = searchParams.search || "";
+    const limit = Number(searchParams.limit) || 15;
+    const page = Number(searchParams.page) || 1;
+    const filter = { search, limit, page };
+
     const ListEmployeeAsync = async () => {
-        const filter = { search };
+        console.log(filter);
+
         const employees = await getEmployeeCollections(filter);
-        return <ListEmployees employees={employees as EmployeeBasic[]} />;
+        return (
+            employees && (
+                <ListEmployees
+                    employees={employees.data as EmployeeBasic[]}
+                    limit={limit}
+                    totalItems={employees.totalItems}
+                />
+            )
+        );
     };
 
     return (
         <PageTemplate title="Trombinoscope">
             <section className="pt-6">
-                <SearchInputDataTable />
+                <div className="flex justify-between items-center">
+                    {" "}
+                    <SearchInputDataTable />
+                </div>
             </section>
             <React.Suspense fallback={<LoadingSkeleton />}>
                 <ListEmployeeAsync />
