5e91edfec09efe4c497512af52b002714567d032
assadi-dev <assadi.halifa@gmail.com>
2024-11-24 18:35:33 +0100

    "Updated GET route in users/collections to accept NextRequest and extract filter params from searchParams"

--- src/app/api/(users)/users/collections/route.ts
+++ src/app/api/(users)/users/collections/route.ts
@@ -1,10 +1,16 @@
 import { getAccountCollections } from "@/database/drizzle/repositories/users";
-import { NextResponse } from "next/server";
+import { ExtractFilterParams } from "@/database/drizzle/utils";
+import { NextRequest, NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
     try {
-        const response = await getAccountCollections();
+        const {
+            nextUrl: { searchParams },
+        } = request;
+
+        const filter = ExtractFilterParams(searchParams);
+        const response = await getAccountCollections(filter);
 
         return NextResponse.json(response);
     } catch (error: any) {
