ce70719b20c1773f0411106034078a30d86b4a25
assadi-dev <assadi.halifa@gmail.com>
2024-11-30 22:16:21 +0100

    ajout wip

--- src/app/(private)/tableau-de-board/_dashboard/sections/Announcement/index.tsx
+++ src/app/(private)/tableau-de-board/_dashboard/sections/Announcement/index.tsx
@@ -1,11 +1,20 @@
-import { Card } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { wait } from "@/lib/utils";
+import { Cog } from "lucide-react";
 import React from "react";
 
 const Announcement = () => {
     return (
-        <Card className="bg-primary ">
-            Announcement <button type="button">test</button>
+        <Card className="bg-primary text-white">
+            <CardHeader className="text-center mb-3">
+                <CardTitle>
+                    <p className="text-xl">Annonces</p>
+                </CardTitle>
+            </CardHeader>
+            <div className=" flex flex-col justify-center items-center gap-3">
+                <Cog className="w-24 h-24 animate-spin " style={{ animationDuration: "5s" }} />
+                <p className="text-white text-xl font-bold">En cours de développement</p>
+            </div>
         </Card>
     );
 };
