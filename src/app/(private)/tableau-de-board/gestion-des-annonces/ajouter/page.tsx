ba665570d453314cb9163878335c37f66fa9d7ef
assadi-dev <assadi.halifa@gmail.com>
2024-12-06 21:43:23 +0100

    `Removed unnecessary line break and changed export syntax in CreateAnnouncementPage function`

--- src/app/(private)/tableau-de-board/gestion-des-annonces/ajouter/page.tsx
+++ src/app/(private)/tableau-de-board/gestion-des-annonces/ajouter/page.tsx
@@ -7,8 +7,7 @@ import EditorPanel from "../_components/EditorPanel";
 import CanvasContainer from "../_components/CanvasContainer";
 
 export const metadata = setTitlePage("Gestion des annonces");
-
-export const CreateAnnouncementPage = async () => {
+const CreateAnnouncementPage = async () => {
     return (
         <PageTemplate title="Création de l'annonce">
             <div className="grid grid-cols-[auto,1fr,auto] pt-6 h-[78vh] w-full gap-3">
