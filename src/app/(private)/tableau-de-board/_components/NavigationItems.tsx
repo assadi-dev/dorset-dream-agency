13477b3dcb71c54e37311e40aac087bbe7797b8a
assadi-dev <assadi.halifa@gmail.com>
2024-12-12 22:37:37 +0100

    active item sur la sidebar

--- src/app/(private)/tableau-de-board/_components/NavigationItems.tsx
+++ src/app/(private)/tableau-de-board/_components/NavigationItems.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { DashboardNavigationType } from "./types";
 import {
     SidebarMenu,
@@ -12,18 +12,41 @@ import {
 } from "@/components/ui/sidebar";
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
 import { ChevronRight } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 export type NavigationProps = {
     route: DashboardNavigationType;
 };
+const SIDEBAR_STORAGE = "sidebar_toggle";
 
 const NavigationItems = ({ route }: NavigationProps) => {
+    const pathname = usePathname();
+    const storedSidebarState = localStorage.getItem(SIDEBAR_STORAGE);
+    const [state, setState] = useState<{ isOpen: boolean; group: string }>({
+        isOpen: storedSidebarState ? JSON.parse(storedSidebarState).isOpen : false,
+        group: storedSidebarState ? JSON.parse(storedSidebarState).group : "",
+    });
+
+    const isActive = (path: string) => {
+        if (pathname === path) {
+            return true;
+        }
+        return false;
+    };
+
+    const childrenActive = (title: string) => {
+        localStorage.setItem(SIDEBAR_STORAGE, JSON.stringify({ group: title, isOpen: !state.isOpen }));
+        setState((current) => {
+            return { ...current, group: title, isOpen: !current.isOpen };
+        });
+    };
+
     return (
         <>
             {route.children ? (
-                <Collapsible className="group/collapsible">
+                <Collapsible className="group/collapsible" open={state.group == route.title && state.isOpen}>
                     <SidebarMenuItem>
-                        <CollapsibleTrigger asChild>
+                        <CollapsibleTrigger asChild onClick={() => childrenActive(route.title)}>
                             <SidebarMenuButton>
                                 {route.icon && <route.icon />}
                                 <span>{route.title}</span>
@@ -33,8 +56,8 @@ const NavigationItems = ({ route }: NavigationProps) => {
                         <CollapsibleContent className=" border-l border-white/25  ml-3.5">
                             {route.children?.map((parent) =>
                                 parent.path ? (
-                                    <SidebarMenuSubItem>
-                                        <SidebarMenuSubButton asChild>
+                                    <SidebarMenuSubItem key={parent.path}>
+                                        <SidebarMenuSubButton asChild isActive={isActive(parent.path)}>
                                             <a href={parent.path}>
                                                 <span>{parent.title}</span>{" "}
                                             </a>
@@ -47,7 +70,7 @@ const NavigationItems = ({ route }: NavigationProps) => {
                 </Collapsible>
             ) : route.path ? (
                 <SidebarMenuItem>
-                    <SidebarMenuButton asChild>
+                    <SidebarMenuButton asChild isActive={isActive(route.path)}>
                         <Link href={route.path}>
                             {route.icon && <route.icon />}
                             <span>{route.title}</span>
