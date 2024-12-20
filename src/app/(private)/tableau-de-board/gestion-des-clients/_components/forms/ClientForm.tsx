1d9fd1b02687754c43f3df081ce011a38193b778
assadi-dev <assadi.halifa@gmail.com>
2024-12-21 00:45:02 +0100

    Update ClientForm.tsx

--- src/app/(private)/tableau-de-board/gestion-des-clients/_components/forms/ClientForm.tsx
+++ src/app/(private)/tableau-de-board/gestion-des-clients/_components/forms/ClientForm.tsx
@@ -14,6 +14,9 @@ import FormFieldInput from "@/components/forms/FormFieldInput";
 import FormFieldSelect from "@/components/forms/FormFieldSelect";
 import { GENRE_OPTIONS } from "@/config/enums";
 import FormFieldSwitch from "@/components/forms/FormFieldSwitch";
+import { useSession } from "next-auth/react";
+import { UserSession } from "@/auth";
+import { cn, isAdmin } from "@/lib/utils";
 
 type ClientFormProps = React.FormHTMLAttributes<HTMLFormElement> & {
     defaultValues?: ClientFormType;
@@ -22,6 +25,9 @@ type ClientFormProps = React.FormHTMLAttributes<HTMLFormElement> & {
 const ClientForm = ({ defaultValues, save, ...props }: ClientFormProps) => {
     const [isPending, startTransition] = React.useTransition();
     const modalState = useModalState();
+
+    const { data } = useSession();
+    const session = data as UserSession;
 
     const form = useForm<ClientFormType>({
         resolver: zodResolver(clientFormSchema),
@@ -55,26 +61,35 @@ const ClientForm = ({ defaultValues, save, ...props }: ClientFormProps) => {
 
     return (
         <Form {...form}>
-            <form {...props} onSubmit={form.handleSubmit(submitData)}>
-                <div className="mb-4 grid grid-cols-2 gap-3">
-                    <FormFieldInput control={form.control} name="lastName" label="Nom" />
-                    <FormFieldInput control={form.control} name="firstName" label="Prénom" />
+            <form
+                {...props}
+                className={cn(props.className, "flex flex-col justify-between")}
+                onSubmit={form.handleSubmit(submitData)}
+            >
+                <div>
+                    <div className="mb-4 grid grid-cols-2 gap-3">
+                        <FormFieldInput control={form.control} name="lastName" label="Nom" />
+                        <FormFieldInput control={form.control} name="firstName" label="Prénom" />
+                    </div>
+
+                    <div className="mb-4">
+                        <FormFieldInput control={form.control} name="phone" label="N° Téléphone" />
+                    </div>
+                    <div className="mb-5">
+                        <FormFieldSelect control={form.control} label="Genre" name="gender" options={GENRE_OPTIONS} />
+                    </div>
+                    {isAdmin(session.user.role) ? (
+                        <div className="mb-4">
+                            <FormFieldSwitch
+                                control={form.control}
+                                label="Décédé"
+                                name="isDead"
+                                description="Définir si ce client est décédé ou pas"
+                            />
+                        </div>
+                    ) : null}
                 </div>
 
-                <div className="mb-4">
-                    <FormFieldInput control={form.control} name="phone" label="N° Téléphone" />
-                </div>
-                <div className="mb-5">
-                    <FormFieldSelect control={form.control} label="Genre" name="gender" options={GENRE_OPTIONS} />
-                </div>
-                <div className="mb-4">
-                    <FormFieldSwitch
-                        control={form.control}
-                        label="Décédé"
-                        name="isDead"
-                        description="Définir si ce client est décédé ou pas"
-                    />
-                </div>
                 <DialogFooter className="pt-3">
                     <SubmitButton isLoading={isPending} className="mx-auto w-full" type="submit">
                         {SUBMIT_LABEL}
