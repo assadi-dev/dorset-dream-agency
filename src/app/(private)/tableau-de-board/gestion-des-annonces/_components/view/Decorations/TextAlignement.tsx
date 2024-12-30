830a3792e152445c9391a492eb19979560cbe651
assadi-dev <assadi.halifa@gmail.com>
2024-12-30 20:57:48 +0100

    Alignement des textes

--- src/app/(private)/tableau-de-board/gestion-des-annonces/_components/view/Decorations/TextAlignement.tsx
+++ src/app/(private)/tableau-de-board/gestion-des-annonces/_components/view/Decorations/TextAlignement.tsx
@@ -2,26 +2,36 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 import { AlignLeft, AlignRight, AlignCenter, AlignJustify } from "lucide-react";
 import useFabricAction from "../../fabric/useFabric";
+import { getActiveObjectFromLayers } from "../../fabric/helpers";
+import { IText, Textbox } from "fabric";
 
 const TextAlignement = () => {
-    const { canvas, selected } = useFabricAction();
+    const { canvas, selected, updateObject } = useFabricAction();
 
-    const type = selected?.type;
-
-    const handleClickAlign = () => {};
+    const handleClickAlign = (event: React.MouseEvent<HTMLButtonElement>) => {
+        if (!canvas) return;
+        if (!selected?.id) return;
+        const object = getActiveObjectFromLayers(selected.id, canvas);
+        const value = event.currentTarget.dataset?.align;
+        if (object instanceof IText || object instanceof Textbox) {
+            object.set("textAlign", value);
+            canvas.requestRenderAll();
+            updateObject(object);
+        }
+    };
 
     return (
         <>
-            <Button size={"icon"} data-position="left" variant="outline" type="button" onClick={handleClickAlign}>
+            <Button size={"icon"} data-align="left" variant="outline" type="button" onClick={handleClickAlign}>
                 <AlignLeft />
             </Button>
-            <Button size={"icon"} data-position="center" variant="outline" type="button" onClick={handleClickAlign}>
+            <Button size={"icon"} data-align="center" variant="outline" type="button" onClick={handleClickAlign}>
                 <AlignCenter />
             </Button>
-            <Button size={"icon"} data-position="right" variant="outline" type="button" onClick={handleClickAlign}>
+            <Button size={"icon"} data-align="right" variant="outline" type="button" onClick={handleClickAlign}>
                 <AlignRight />
             </Button>
-            {/*  <Button size={"icon"} data-position="justify" variant="outline" type="button" onClick={handleClickAlign}>
+            {/*  <Button size={"icon"} data-align="justify" variant="outline" type="button" onClick={handleClickAlign}>
         <AlignJustify />
     </Button> */}
         </>
