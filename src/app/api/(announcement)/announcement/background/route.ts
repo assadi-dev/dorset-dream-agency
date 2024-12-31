afb8b8ebb18785b95b5a56a156b714a466ff65f0
assadi-dev <assadi.halifa@gmail.com>
2024-12-31 01:06:22 +0100

    `Update route URL in GET function to include announcement/background path`

--- src/app/api/(announcement)/announcement/background/route.ts
+++ src/app/api/(announcement)/announcement/background/route.ts
@@ -13,7 +13,7 @@ type BackgroundFile = {
 
 export const GET = async (request: NextRequest) => {
     try {
-        const url = ENV.DOMAIN + "/api";
+        const url = ENV.DOMAIN + "/api/announcement/background";
         const fileUrl = `${url}`;
         const backgroundDir = path.join(BACKGROUND_DIR_IMAGES);
         const promise = (): Promise<BackgroundFile[]> =>
