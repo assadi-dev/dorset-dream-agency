9711c1119319960c366ec5fc8d4ad25ed4293259
assadi-dev <assadi.halifa@gmail.com>
2025-01-04 20:46:39 +0100

    update

---
+++ src/app/api/(announcement)/announcements/saves/[key]/route.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from "next/server";
+import path from "path";
+import * as fs from "fs";
+import { UPLOAD_ANNOUNCEMENT_DIR_SAVES } from "@/config/dir";
+import { findFileByPath } from "@/database/drizzle/repositories/files";
+
+export const dynamic = "force-dynamic";
+
+type Params = {
+    params: {
+        key: string;
+    };
+};
+
+export async function GET(req: NextRequest, { params: { key } }: Params) {
+    try {
+        if (!key) throw new Error("Key undefined");
+
+        const filePath = path.join(UPLOAD_ANNOUNCEMENT_DIR_SAVES, key);
+        const buffer = fs.readFileSync(filePath);
+        const { size } = fs.statSync(filePath);
+        const file = await findFileByPath(key);
+        const mimeType = file?.mimeType || "application/json";
+
+        const headers = new Headers({
+            "Content-Type": mimeType,
+            "Content-Length": size.toString(),
+        });
+        const response = new NextResponse(buffer, {
+            status: 200,
+            headers,
+        });
+        return response;
+    } catch (error: any) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                { message: error.message },
+                {
+                    status: 500,
+                },
+            );
+        }
+    }
+}
