bfc4dd27c672d4220b609aaafb3004161b22c65a
assadi-dev <assadi.halifa@gmail.com>
2025-02-02 00:41:47 +0100

    `Update userId variable in deleteEmployee function to use account.userId instead of users.id`

--- src/database/drizzle/repositories/employee.ts
+++ src/database/drizzle/repositories/employee.ts
@@ -220,7 +220,7 @@ export const deleteEmployee = async (ids: Array<number>) => {
                     .prepare();
 
                 await request?.execute({
-                    userId: users.id,
+                    userId: account?.userId,
                 });
             }
 
