79bafabeaa099d4ed7ebbaad96c6490b4a73b8f4
assadi-dev <assadi.halifa@gmail.com>
2025-03-07 19:38:56 +0100

    `Added dropdown menu with delete action for selected items in ListLocation component`

--- src/app/(private)/tableau-de-board/gestion-des-locations-et-ventes/_components/ListLocation.tsx
+++ src/app/(private)/tableau-de-board/gestion-des-locations-et-ventes/_components/ListLocation.tsx
@@ -8,6 +8,16 @@ import { CellColumn } from "@/app/types/ReactTable";
 import SimplePagination from "@/components/Paginations/SimplePagination";
 import CheckBoxColumn from "@/components/Datatable/CheckBoxColumn";
 import useSelectTableRow from "@/hooks/useSelectTableRow";
+import { Trash, Trash2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import ButtonActionWithTooltip from "@/components/Buttons/ButtonActionWithTooltip";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 type ListLocationProps = {
     transactions: any[];
@@ -37,7 +47,35 @@ const ListLocation = ({ transactions, limit, totalItems }: ListLocationProps) =>
     return (
         <>
             <div className="my-5 flex justify-between items-center">
-                <div></div>
+                <div className="flex items-center gap-2">
+                    {" "}
+                    {itemChecked.length > 0 && (
+                        <div className="flex gap-3 items-center ">
+                            <p className="px-2 py-0.5 bg-slate-50 ring-1 ring-slate-300 rounded-md shadow">
+                                {" "}
+                                {itemChecked.length} element sélectionnées{" "}
+                            </p>{" "}
+                            <DropdownMenu>
+                                <DropdownMenuTrigger>
+                                    <Button size={"sm"} variant={"outline"}>
+                                        Actions
+                                    </Button>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent>
+                                    <DropdownMenuItem asChild={true}>
+                                        <Button
+                                            size={"sm"}
+                                            className="flex items-center gap-2 w-full"
+                                            variant={"ghost"}
+                                        >
+                                            <Trash2 /> Supprimer
+                                        </Button>
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </div>
+                    )}{" "}
+                </div>
                 <SimplePagination limit={limit} totalItems={totalItems} />
             </div>
             <DataTable columns={transactionsColumn} data={transactions} />
