4156eb8fc385c9086315fd883a65dbc288e2abf1
assadi-dev <assadi.halifa@gmail.com>
2025-03-09 17:17:13 +0100

    `Refactored NavbarCatalogue component: replaced Button with Link for logo`

--- src/app/(catalogue)/_components/header/NavbarCatalogue.tsx
+++ src/app/(catalogue)/_components/header/NavbarCatalogue.tsx
@@ -29,18 +29,16 @@ const NavbarCatalogue = async () => {
         <nav className="w-full  min-h-10 sm:px-8 2xl:max-w-[1800px] mx-auto pt-3 px-3 sm:pt-5  ">
             <div className="shadow bg-[#106835] backdrop-blur-lg transition-all grid grid-cols-[1fr,0.5fr] rounded-lg p-3 items-center">
                 <div className="h-[50px] flex items-center">
-                    <Button asChild variant="ghost" type="button">
-                        <Link href="/">
-                            {/*  <strong>DORSET DREAM</strong> */}
-                            <Image
-                                src={logo}
-                                alt="logo du site catalogue Dorset dream agency"
-                                height={1200}
-                                width={800}
-                                className="h-auto  w-[4rem] lg:w-[5rem] "
-                            />
-                        </Link>
-                    </Button>
+                    <Link href="/" className="px-5">
+                        {/*  <strong>DORSET DREAM</strong> */}
+                        <Image
+                            src={logo}
+                            alt="logo du site catalogue Dorset dream agency"
+                            height={1200}
+                            width={800}
+                            className="h-auto  w-[4rem] lg:w-[5rem] "
+                        />
+                    </Link>
                 </div>
                 <div className="flex justify-end items-center">
                     <ConnectButton />
