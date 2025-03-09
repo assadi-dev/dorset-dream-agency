bdb271b3eb216afbed6abbd6c979f7ba12668344
assadi-dev <assadi.halifa@gmail.com>
2025-03-09 16:36:42 +0100

    `Update PageTopLoader component with new color and shadow values`

--- src/components/loader/PageTopLoader.tsx
+++ src/components/loader/PageTopLoader.tsx
@@ -3,7 +3,7 @@ import NextTopLoader from "nextjs-toploader";
 const PageTopLoader = () => {
     return (
         <NextTopLoader
-            color="#2299DD"
+            color="#106835"
             initialPosition={0.08}
             crawlSpeed={200}
             height={3}
@@ -11,7 +11,7 @@ const PageTopLoader = () => {
             showSpinner={false}
             easing="ease"
             speed={200}
-            shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+            shadow="0 0 10px #106835,0 0 5px #106835"
             template='<div class="bar" role="bar"><div class="peg"></div></div> 
     <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
             zIndex={1600}
