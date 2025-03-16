0e5ff73bbf07f30ee9898f685c80e275b6dbce0f
assadi-dev <assadi.halifa@gmail.com>
2025-03-16 15:06:57 +0100

    re-design des badge card

--- src/app/(catalogue)/_components/main/PropertyBadge.tsx
+++ src/app/(catalogue)/_components/main/PropertyBadge.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/lib/utils";
+import { CircleCheck, CircleCheckBig, CircleX, SofaIcon, Weight } from "lucide-react";
 import React from "react";
 type PropertyBadgesProps = {
     isAvailable: boolean;
@@ -9,36 +11,49 @@ export const FurnishBadge = ({ isFurnish }: { isFurnish: boolean }) => {
     const FURNISH_LABEL_ = isFurnish ? "Meublé" : "Vide";
 
     return (
-        <span className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-400">
-            {FURNISH_LABEL_}
-        </span>
+        <p className="flex items-center gap-1  text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded  border border-gray-400">
+            <SofaIcon className="h-3.5 w-3.5 inline-block" />
+            <span> {FURNISH_LABEL_}</span>
+        </p>
     );
 };
 
 export const AvailableBadge = ({ isAvailable }: { isAvailable: boolean }) => {
     const AVAILABLE_LABEL = isAvailable ? "Disponible" : "Indisponible";
 
-    const CLASS_BADGE = isAvailable
-        ? `bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400`
-        : "bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400";
-    return <span className={CLASS_BADGE}>{AVAILABLE_LABEL}</span>;
+    const CLASS_BADGE = `flex items-center gap-1 bg-green-100 text-green-800 text-xs font-medium me-2 px-1.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400`;
+    const ICON_CLASS = `h-3.5 w-3.5`;
+    return (
+        <p
+            className={cn(CLASS_BADGE, {
+                "bg-red-100 text-red-800": !isAvailable,
+                "text-red-800": !isAvailable,
+                "dark:text-red-400": !isAvailable,
+                "border border-red-400": !isAvailable,
+            })}
+        >
+            {isAvailable ? <CircleCheck className={ICON_CLASS} /> : <CircleX className={ICON_CLASS} />}
+            {AVAILABLE_LABEL}
+        </p>
+    );
 };
 
 export const StockageBadges = ({ stock }: { stock: number | null }) => {
     const STOCK_LABEL = (stock && stock > 0 && `${stock} kg`) || "Pas de coffre";
     return (
-        <span className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-400">
-            {STOCK_LABEL}
-        </span>
+        <p className="flex items-center gap-1  text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded border border-gray-400">
+            <Weight className="w-3 h-3" />
+            <span>{STOCK_LABEL}</span>
+        </p>
     );
 };
 
 export const PropertyBadges = ({ isAvailable, isFurnish, stock }: PropertyBadgesProps) => {
     return (
-        <div className="flex items-center gap-1 flex-wrap justify-center lg:justify-start">
+        <div className="flex items-center gap-1 flex-wrap ">
             <FurnishBadge isFurnish={isFurnish} />
-            <AvailableBadge isAvailable={isAvailable} />
             {typeof stock === "number" && stock >= 0 ? <StockageBadges stock={stock} /> : null}
+            {/*   <AvailableBadge isAvailable={isAvailable} /> */}
         </div>
     );
 };
--- src/app/(catalogue)/_components/main/PropertyCard.tsx
+++ src/app/(catalogue)/_components/main/PropertyCard.tsx
@@ -4,6 +4,8 @@ import { Card } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import { PropertyBadges } from "./PropertyBadge";
+import { HandCoins, Handshake } from "lucide-react";
+import formatThousands from "format-thousands";
 
 type PropertyItemType = {
     id: number;
@@ -26,7 +28,7 @@ type PropertyCardProps = {
 const PropertyCard = ({ property }: PropertyCardProps) => {
     const sellingPrice =
         property.sellingPrice !== -1 ? (
-            property.sellingPrice + "$"
+            formatThousands(property.sellingPrice) + "$"
         ) : (
             <span className="px-2 py-1 ring-1 ring-destructive rounded text-xs lg:text-sm  text-destructive bg-red-100">
                 Non achetable
@@ -55,21 +57,31 @@ const PropertyCard = ({ property }: PropertyCardProps) => {
                     stock={property.stock}
                 />
                 <div className="flex flex-col lg:flex-row items-center justify-between  p-2 rounded-sm backdrop-blur-md w-full gap-3">
-                    <div className="flex flex-col gap-1">
-                        <p className="text-xs text-slate-600">Location - Vente</p>
-                        <p className="font-bold text-xs lg:text-lg">
-                            {property.rentalPrice}$ - {sellingPrice}
+                    <div className="flex flex-col gap-1 w-full">
+                        <p className="text-sm text-slate-600 flex justify-between items-center w-full">
+                            <span>
+                                {" "}
+                                <HandCoins className="w-4 h-4 inline-block mr-1" /> Location:{" "}
+                            </span>
+                            <span className="font-semibold">{formatThousands(property.rentalPrice)}$</span>
+                        </p>
+                        <p className="text-sm text-slate-600 flex justify-between items-center w-full">
+                            <span>
+                                {" "}
+                                <Handshake className="w-4 h-4 inline-block mr-1" /> Vente:{" "}
+                            </span>
+                            <span className="font-semibold">{sellingPrice}</span>
                         </p>
                     </div>
-                    <Button
-                        asChild
-                        variant={"default"}
-                        type="button"
-                        className="bg-gradient-to-br from-black to-primary !text-white lg:bg-background border hover:border-none hover:shadow-lg lg:hover:shadow-blue-950/50 border-input lg:text-black hover:bg-gradient-to-br   duration-300   transition-all  lg:hover:text-white text-xs lg:text-sm w-full lg:w-fit"
-                    >
-                        <Link href={`property?id=${property.id}&name=${property.name}`}>Plus d'info</Link>
-                    </Button>
                 </div>
+                <Button
+                    asChild
+                    variant={"default"}
+                    type="button"
+                    className="bg-gradient-to-br from-black to-primary !text-white lg:bg-background border hover:border-none hover:shadow-lg lg:hover:shadow-blue-950/50 border-input lg:text-black hover:bg-gradient-to-br   duration-300   transition-all  lg:hover:text-white text-xs lg:text-sm w-full "
+                >
+                    <Link href={`property?id=${property.id}&name=${property.name}`}>Plus d'info</Link>
+                </Button>
             </div>
         </Card>
     );
