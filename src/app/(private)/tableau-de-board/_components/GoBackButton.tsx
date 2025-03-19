8842fc8328b903ff9221613d543bf3580c5b5efd
assadi-dev <assadi.halifa@gmail.com>
2025-03-19 10:45:21 +0100

    Update GoBackButton.tsx

--- src/app/(private)/tableau-de-board/_components/GoBackButton.tsx
+++ src/app/(private)/tableau-de-board/_components/GoBackButton.tsx
@@ -12,7 +12,7 @@ const GoBackButton = () => {
                 variant="outline"
                 size="sm"
                 onClick={() => router.back()}
-                className="bg-primary drop-shadow-lg flex items-center border border-black/25 hover:underline"
+                className="bg-gradient-to-br from-primary-accent to-white shadow-lg flex items-center border border-black/25 hover:underline"
             >
                 <ChevronLeftCircle className="h-10 w-10" /> Précédent
             </Button>
