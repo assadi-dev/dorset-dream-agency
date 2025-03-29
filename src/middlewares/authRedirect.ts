599214bfc65f21dec68ec3ea7e2492eabef68a8f
assadi-dev <assadi.halifa@gmail.com>
2025-03-29 17:00:49 +0100

    redirection depuis middleware

---
+++ src/middlewares/authRedirect.ts
@@ -0,0 +1,17 @@
+import { ENV } from "@/config/global";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export const authRedirect = async (request: NextRequest) => {
+    const cookie = cookies();
+    const session_token = cookie.get("authjs.session-token")?.value;
+    const LOGIN_PAGE = ENV.NEXT_AUTH_SIGN_IN_PAGE;
+
+    if (
+        !session_token &&
+        request.nextUrl.pathname.includes("tableau-de-board") &&
+        !request.nextUrl.pathname.includes(LOGIN_PAGE)
+    )
+        return NextResponse.redirect(new URL(LOGIN_PAGE, request.url));
+    return NextResponse.next();
+};
