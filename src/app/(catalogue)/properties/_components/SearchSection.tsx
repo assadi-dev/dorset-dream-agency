0717d453d3fbdcd39b87260f4fcf923fa1f96f50
assadi-tkorp <assadi.halifa@gmail.com>
2025-08-24 18:08:13 +0200

    Update SearchSection.tsx

--- src/app/(catalogue)/properties/_components/SearchSection.tsx
+++ src/app/(catalogue)/properties/_components/SearchSection.tsx
@@ -46,20 +46,32 @@ const SearchSection = () => {
     return (
         <section className="my-3">
             <div className=" bg-background p-5 flex flex-col gap-3 lg:flex-row items-center rounded-lg shadow-lg min-h-[3rem] justify-between">
-                <form onSubmit={submitSearch} className="w-full flex flex-col gap-3">
-                    <div className="flex gap-3 w-full flex-nowrap">
-                        <Input type="search" placeholder="Rechercher" name="search" defaultValue={searchTermInParam} />
+                <form onSubmit={submitSearch} className="w-full flex flex-col lg:flex-row gap-3">
+                    <div className="flex gap-3 w-full flex-col sm:flex-row">
+                        <div className="w-full">
+                            <p className="font-semibold text-slate-500">Rechercher une propriété</p>
+                            <Input
+                                type="search"
+                                placeholder="Rechercher"
+                                name="search"
+                                defaultValue={searchTermInParam}
+                            />
+                        </div>
 
-                        <div className="w-2/3">
+                        <div className="w-full lg:w-2/3">
+                            <p className="font-semibold text-slate-500">Catégorie</p>
                             <SelectCategory />
                         </div>
                     </div>
-                    <div className="flex flex-col sm:flex-row gap-3 items-center">
-                        <SelectAvailability />
+                    <div className="flex flex-col sm:flex-row gap-3 items-center w-full">
+                        <div className="w-full">
+                            <p className="font-semibold text-slate-500">Disponibilité</p>
+                            <SelectAvailability />
+                        </div>
 
                         <Button
                             type="submit"
-                            className="active:scale-90 transition-all duration-300 bg-primary w-full flex gap-2 items-center"
+                            className="active:scale-90 transition-all duration-300 bg-primary w-full flex gap-2 items-center self-end"
                         >
                             <Search /> Rechercher
                         </Button>
