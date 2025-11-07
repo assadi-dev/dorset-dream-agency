9be80b64a460a309d778a641c7eb4d8e47996e48
assadi-dev <assadi.halifa@gmail.com>
2025-11-07 15:40:02 +0100

    fix scroll zone d'edition

--- src/components/Text/RichTextEditor/RichTextEditor.tsx
+++ src/components/Text/RichTextEditor/RichTextEditor.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEditor, EditorContent, findParentNode, posToDOMRect, useEditorState } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import { ScrollArea } from "../../ui/scroll-area";
+import { ScrollArea, ScrollBar } from "../../ui/scroll-area";
 import HeaderRichtext from "./components/HeaderRichtext";
 import { TiptapContent } from "./type";
 import TextAlign from "@tiptap/extension-text-align";
@@ -27,7 +27,7 @@ export function RichTextEditor({ content, onChange }: RichTextEditorProps) {
         },
         editorProps: {
             attributes: {
-                class: "prose prose-sm max-w-none focus:outline-none min-h-[300px] p-4",
+                class: "prose prose-sm max-w-full  focus:outline-none min-h-[300px] px-4",
             },
         },
     });
@@ -39,9 +39,10 @@ export function RichTextEditor({ content, onChange }: RichTextEditorProps) {
     return (
         <div className="border rounded-lg overflow-hidden">
             <HeaderRichtext editor={editor} />
-            <ScrollArea className="max-h-[35vh]">
+            <ScrollArea className="h-[35vh] pb-2">
                 <BubbleMenuRow editor={editor} />
-                <EditorContent editor={editor} className="bg-card" />
+                <EditorContent editor={editor} className="bg-card cursor-text" />
+                <ScrollBar className="h-0.5" orientation="vertical" />
             </ScrollArea>
         </div>
     );
