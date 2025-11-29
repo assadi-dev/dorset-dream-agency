751f17ec3dd3fe72b2f95708c98666b860eed362
assadi-dev <assadi.halifa@gmail.com>
2025-11-29 20:21:02 +0100

    amelioration visuel  du isCover

--- src/app/(private)/tableau-de-board/gestion-des-biens-immobiliers/ajouter/_components/views/Button.tsx
+++ src/app/(private)/tableau-de-board/gestion-des-biens-immobiliers/ajouter/_components/views/Button.tsx
@@ -2,17 +2,28 @@ import React from "react";
 import { BookImage } from "lucide-react";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
-export const CoverButton = ({ onClick }: { onClick: () => void }) => {
+export const CoverButton = ({ isCover, onClick }: { isCover: boolean; onClick: () => void }) => {
+    const BTN_CLASS =
+        "rounded-full w-6 h-6 !text-black !bg-gradient-to-br from-primary-accent  text-primary-accent active:scale-110 transition-all  opacity-0 group-hover:opacity-100";
+
     return (
         <Tooltip>
             <TooltipTrigger asChild>
                 <Button
                     variant={"outline"}
                     type="button"
-                    //tooltipTitle="Définir cette image en couverture"
                     size={"icon"}
-                    className="rounded-full w-6 h-6 !text-black !bg-gradient-to-br from-primary-accent  text-primary-accent active:scale-110 transition-all  opacity-0 group-hover:opacity-100"
+                    className={cn(BTN_CLASS, {
+                        "ring-green-900": isCover,
+                        "ring-1": isCover,
+                        "!text-green-950": isCover,
+                        "!from-lime-400": isCover,
+                        "!to-lime-400": isCover,
+                        "opacity-100": isCover,
+                        "hover:bg-lime-400": isCover,
+                    })}
                     onClick={onClick}
                 >
                     <BookImage className="p-0" />
--- src/app/(private)/tableau-de-board/gestion-des-biens-immobiliers/ajouter/_components/views/PreviewVarianteUpload.tsx
+++ src/app/(private)/tableau-de-board/gestion-des-biens-immobiliers/ajouter/_components/views/PreviewVarianteUpload.tsx
@@ -4,16 +4,28 @@ import Image from "next/image";
 import { Check } from "lucide-react";
 import { CoverButton } from "./Button";
 import { LoadPreviewFile } from "@/lib/client_side";
+import { is } from "drizzle-orm";
 
 type PreviewVarianteUploadType = {
+    selectedMode: boolean;
+    isSelected: boolean;
     isCover: boolean;
     order?: number;
     file?: FileObj & { originalName?: string };
     onRemove?: (file: FileObj) => void;
     setCover?: (file: FileObj) => void;
+    onSelected?: (id: number) => void;
 };
 
-const PreviewVarianteUpload = ({ isCover, file, onRemove, setCover }: PreviewVarianteUploadType) => {
+const PreviewVarianteUpload = ({
+    selectedMode,
+    isSelected,
+    isCover,
+    file,
+    onRemove,
+    setCover,
+    onSelected,
+}: PreviewVarianteUploadType) => {
     //onClick={() => onRemove && onRemove()}
     const handleClickSetCover = React.useCallback(() => {
         if (!file) return;
@@ -24,16 +36,29 @@ const PreviewVarianteUpload = ({ isCover, file, onRemove, setCover }: PreviewVar
     const IsDefaultCover = () => {
         return (
             <div className="absolute bg-lime-300 text-green-950 ring-1 ring-green-900 rounded m-1 z-50">
-                <Check className="h-5 w-5 p-0.5" />{" "}
+                {/*  <Check className="h-5 w-5 p-0.5" />{" "} */}
+                cover
             </div>
         );
     };
 
     const cover = LoadPreviewFile(file?.url as string);
 
+    const handleSelected = React.useCallback(
+        (id: number) => {
+            if (onSelected) {
+                onSelected(id);
+            }
+        },
+        [onSelected],
+    );
+
     return (
         <div className="relative w-full h-[110px] rounded overflow-hidden group z-0">
-            {isCover && <IsDefaultCover />}
+            {selectedMode && file?.id && (
+                <SelectMode id={String(file?.id)} isSelected={isSelected} onSelected={handleSelected} />
+            )}
+
             {file && (
                 <Image
                     src={cover}
@@ -46,7 +71,7 @@ const PreviewVarianteUpload = ({ isCover, file, onRemove, setCover }: PreviewVar
             )}
             <div className="absolute top-0 left-0 bottom-0 w-full   group-hover:bg-gradient-to-b from-black/80 to-primary/50 transition-all motion-preset-slide-right z-100">
                 <div className="flex justify-end items-center p-2">
-                    <CoverButton onClick={handleClickSetCover} />
+                    <CoverButton isCover={isCover ?? false} onClick={handleClickSetCover} />
                 </div>
             </div>
         </div>
@@ -54,3 +79,27 @@ const PreviewVarianteUpload = ({ isCover, file, onRemove, setCover }: PreviewVar
 };
 
 export default PreviewVarianteUpload;
+
+type SelectModeProps = { id: string; isSelected: boolean; onSelected?: (id: number) => void };
+const SelectMode = ({ id, isSelected, onSelected }: SelectModeProps) => {
+    const handleSelected = React.useCallback(
+        (id: number) => {
+            if (onSelected) {
+                onSelected(id);
+            }
+        },
+        [onSelected],
+    );
+
+    return (
+        <div className="absolute top-0 left-0 bottom-0 w-full h-full flex justify-center items-center z-50">
+            <input
+                type="checkbox"
+                id={`select-${id}`}
+                checked={isSelected}
+                onChange={() => handleSelected(Number(id))}
+                className="w-5 h-5 rounded-full border-2 border-white bg-transparent checked:bg-primary checked:border-primary"
+            />
+        </div>
+    );
+};
