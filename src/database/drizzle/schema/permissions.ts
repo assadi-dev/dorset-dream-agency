39c8167add77653fc3d0db8362ac781b3d9e003f
assadi-dev <assadi.halifa@gmail.com>
2025-12-11 00:12:05 +0100

    Update permissions.ts

--- src/database/drizzle/schema/permissions.ts
+++ src/database/drizzle/schema/permissions.ts
@@ -1,14 +1,13 @@
 import { relations } from "drizzle-orm";
-import { deletedAt, updatedAndCreatedAt } from "../utils";
-import { int, mysqlEnum, mysqlTable, text, varchar } from "drizzle-orm/mysql-core";
-import { employees } from "./employees";
-import { ACTIONS } from "@/lib/rbac/constants";
+import { updatedAndCreatedAt } from "../utils";
+import { int, mysqlTable, text, varchar } from "drizzle-orm/mysql-core";
 import { rolePermissions } from "./rolePermissions";
 
 export const permissions = mysqlTable("permissions", {
     id: int("id").autoincrement().primaryKey(),
     name: varchar("name", { length: 100 }).notNull(),
     displayName: varchar("display_name", { length: 100 }).notNull(),
+    description: text("description"),
     ressource: varchar("ressource", { length: 50 }).notNull(), //Entity: properties,users,etc
     action: varchar("action", { length: 100 }).notNull(), //Actions : read,create, update,delete
     ...updatedAndCreatedAt,
