8e33a309bfe7f5b2cdab6b11af497ae9efe777ac
assadi-dev <assadi.halifa@gmail.com>
2025-12-11 00:12:08 +0100

    Update permissions.ts

--- src/database/drizzle/repositories/permissions.ts
+++ src/database/drizzle/repositories/permissions.ts
@@ -1,21 +1,50 @@
+import { db } from "@/database";
 import { insertUserAction } from "../sqlite/repositories/usersAction";
 import { ACTION_NAMES, ENTITIES_ENUM } from "../utils";
+import { CreatePermissionInputs, UpdatePermissionInputs } from "./dto/permissionDTO";
 import { generateDescription } from "./utils/entity";
+import { permissions } from "../schema/permissions";
+import { eq, sql } from "drizzle-orm";
 
-export const createPermission = async (inputs) => {
+export const createPermission = async (inputs: CreatePermissionInputs) => {
+    const result = await db.insert(permissions).values(inputs);
+    const permission = await findPermissionById(result[0].insertId);
     if (permission) {
-        const descriptionMessage = `Ajout permission ${permission.name}`;
+        const descriptionMessage = `Ajout permission ${permission.displayName}`;
         registerPermissionAction("create", descriptionMessage, ACTION_NAMES.permissions.create);
     }
 };
-export const findPermissionById = async (id: number) => {};
-export const updatePermission = async (id: number, inputs) => {};
-export const deletePermission = async (id: number) => {
+export const findPermissionById = async (id: number) => {
+    try {
+        const query = db
+            .select()
+            .from(permissions)
+            .where(eq(permissions.id, sql.placeholder("id")))
+            .prepare();
+        const result = await query.execute({ id });
+        if (!result[0]) return null;
+        return result[0];
+    } catch (error: any) {
+        throw error;
+    }
+};
+export const updatePermission = async (id: number, inputs: UpdatePermissionInputs) => {
     const permission = await findPermissionById(id);
     if (!permission) throw Error("permission no found");
 
+    const query = db.update(permissions);
+
     if (permission) {
-        const descriptionMessage = `Suppression de la permission ${permission.name}`;
+        const descriptionMessage = `Modification de la permission ${permission.displayName}`;
+        registerPermissionAction("delete", descriptionMessage, ACTION_NAMES.permissions.delete);
+    }
+};
+export const deletePermission = async (id: number) => {
+    const permission = await findPermissionById(id);
+    if (!permission) throw Error("permission no found");
+    await db.delete(permissions).where(eq(permissions.id, sql.placeholder("id")));
+    if (permission) {
+        const descriptionMessage = `Suppression de la permission ${permission.displayName}`;
         registerPermissionAction("delete", descriptionMessage, ACTION_NAMES.permissions.delete);
     }
 };
