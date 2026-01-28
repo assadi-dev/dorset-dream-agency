256712f0bebf4e80522b35fb1aaa47aaeeca49b7
assadi-dev <assadi.halifa@gmail.com>
2026-01-28 23:06:06 +0100

    fix(gestion-biens): display property name even without variants

--- src/app/(private)/tableau-de-board/gestion-des-biens-immobiliers/_components/RenderPropertyCell.tsx
+++ src/app/(private)/tableau-de-board/gestion-des-biens-immobiliers/_components/RenderPropertyCell.tsx
@@ -29,39 +29,40 @@ export const BadgeRender = ({
     property: { name: string };
     variants: { id: number; name: string }[];
 }) => {
-    const item = variants.length ?? 0;
-    if (item > 0) {
-        return (
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild disabled={item < 1} className="cursor-pointer">
-                    <div className="flex flex-col relative gap-1">
-                        {property.name}
-                        <div className="rounded-full text-xs  text-slate-400 cursor-pointer">
-                            {item} variante{item > 1 ? "s" : null}
-                        </div>
-                    </div>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="max-w-[52vw]">
-                    <DropdownMenuGroup>
-                        <DropdownMenuLabel className="text-xs text-slate-400 text-center">
-                            Variante{item > 1 ? "s" : null}
-                        </DropdownMenuLabel>
-                    </DropdownMenuGroup>
-                    <ScrollArea className=" max-h-[20vh] w-[18vw] overflow-y-auto rounded pb-2">
-                        {variants?.map((v) => (
-                            <DropdownMenuItem
-                                key={v.id}
-                                className="text-nowrap w-full focus:!bg-secondary hover:!bg-secondary bg-secondary my-1 text-xs"
-                            >
-                                {v.name ? v.name : "anonyme"}{" "}
-                            </DropdownMenuItem>
-                        ))}
-                        <ScrollBar orientation="vertical" />
-                    </ScrollArea>
-                </DropdownMenuContent>
-            </DropdownMenu>
-        );
+    const item = variants?.length ?? 0;
+    if (!item) {
+        <div className="flex flex-col relative gap-1">{property.name}</div>;
     }
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild disabled={item < 1} className="cursor-pointer">
+                <div className="flex flex-col relative gap-1">
+                    {property.name}
+                    <div className="rounded-full text-xs  text-slate-400 cursor-pointer">
+                        {item} variante{item > 1 ? "s" : null}
+                    </div>
+                </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="max-w-[52vw]">
+                <DropdownMenuGroup>
+                    <DropdownMenuLabel className="text-xs text-slate-400 text-center">
+                        Variante{item > 1 ? "s" : null}
+                    </DropdownMenuLabel>
+                </DropdownMenuGroup>
+                <ScrollArea className=" max-h-[20vh] w-[18vw] overflow-y-auto rounded pb-2">
+                    {variants?.map((v) => (
+                        <DropdownMenuItem
+                            key={v.id}
+                            className="text-nowrap w-full focus:!bg-secondary hover:!bg-secondary bg-secondary my-1 text-xs"
+                        >
+                            {v.name ? v.name : "anonyme"}{" "}
+                        </DropdownMenuItem>
+                    ))}
+                    <ScrollBar orientation="vertical" />
+                </ScrollArea>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
 
     return null;
 };
