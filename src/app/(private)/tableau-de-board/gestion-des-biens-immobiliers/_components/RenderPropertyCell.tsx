f5a8aec6f834655381125fd0d3373649b7cb9ea1
assadi-dev <assadi.halifa@gmail.com>
2026-01-28 01:53:19 +0100

    feat(properties-ui): add dropdown to display variants

--- src/app/(private)/tableau-de-board/gestion-des-biens-immobiliers/_components/RenderPropertyCell.tsx
+++ src/app/(private)/tableau-de-board/gestion-des-biens-immobiliers/_components/RenderPropertyCell.tsx
@@ -1,28 +1,54 @@
 import React from "react";
 import { PropertiesColumn } from "../types";
 import { Badge } from "@/components/ui/badge";
+import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
+import {
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 
 type RenderPropertyCellProps = {
     property: PropertiesColumn;
 };
 const RenderPropertyCell = ({ property }: RenderPropertyCellProps) => {
-    console.log(property);
-    const variantsSize = property.variants.length ?? 0;
     return (
         <div className="flex flex-col relative gap-1">
-            {property.name} <BadgeRender item={variantsSize} />
+            {property.name} <BadgeRender variants={property.variants} />
         </div>
     );
 };
 
 export default RenderPropertyCell;
 
-export const BadgeRender = ({ item }: { item: number }) => {
+export const BadgeRender = ({ variants }: { variants: { id: number; name: string }[] }) => {
+    const item = variants.length ?? 0;
     if (item > 0) {
         return (
-            <span className="rounded-full text-xs  text-slate-400">
-                {item} variante{item > 1 ? "s" : null}
-            </span>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <div className="rounded-full text-xs  text-slate-400 cursor-pointer">
+                        {item} variante{item > 1 ? "s" : null}
+                    </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="max-w-[52vw]">
+                    <DropdownMenuGroup>
+                        <DropdownMenuLabel className="text-xs text-slate-400 text-center">
+                            Variante{item > 1 ? "s" : null}
+                        </DropdownMenuLabel>
+                    </DropdownMenuGroup>
+                    <DropdownMenuItem className=" flex no-wrap gap-2 focus:!bg-transparent">
+                        {variants?.map((v) => (
+                            <Badge key={v.id} variant="secondary">
+                                {v.name ? v.name : "anonyme"}{" "}
+                            </Badge>
+                        ))}
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
         );
     }
 
