59c73be7792f2031629d5c2a7500bff2f598eb80
assadi-dev <assadi.halifa@gmail.com>
2026-01-29 09:28:12 +0100

    Create page.tsx

---
+++ src/app/(private)/tableau-de-board/developper/release/page.tsx
@@ -0,0 +1,19 @@
+import ModalProvider from "@/components/Modals/ModalProvider";
+import PageTemplate from "../../_components/PageTemplate";
+
+const ReleasePage = () => {
+    return (
+        <ModalProvider>
+            <PageTemplate title="Release" description="Generation de texte d'annonce des versions">
+                <section className="my-3">
+                    <div className="md:grid md:grid-cols-[minmax(100px,0.5fr),1fr] pb-3 items-center">
+                        <div></div>
+                    </div>
+                </section>
+                <section></section>
+            </PageTemplate>
+        </ModalProvider>
+    );
+};
+
+export default ReleasePage;
