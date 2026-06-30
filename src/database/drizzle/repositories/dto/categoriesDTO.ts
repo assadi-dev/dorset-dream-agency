188d78d831005d2914972fe421f09e709304a9a1
assadi-dev <assadi.halifa@gmail.com>
2026-06-30 10:20:38 +0200

    feat: add categories DTO and validation schema for CRUD operations

---
+++ src/database/drizzle/repositories/dto/categoriesDTO.ts
@@ -0,0 +1,25 @@
+
+import { REQUIRE_MESSAGE_ERROR } from "@/config/messages";
+import z from "zod";
+
+export const categoriesSchema = z.object({
+    name: z.string().min(1, { message: REQUIRE_MESSAGE_ERROR }),
+});
+
+export type CategoryInputsType = z.infer<typeof categoriesSchema>;
+
+const idsSchema = z.array(z.number()).min(1, { message: REQUIRE_MESSAGE_ERROR });
+
+export const categoriesValidator  = {
+    create: (data: unknown) => {
+        return categoriesSchema.safeParse(data);
+    },
+    update: (data: unknown) => {
+        return categoriesSchema.safeParse(data);
+    },
+    deleteMany: (ids: number[]) => {
+        return z.object({
+            ids: idsSchema,
+        }).safeParse(ids);
+    },
+}
